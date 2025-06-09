c758cf53c7da602ed2a980cdd2648efa76e06dd1
Marcelo Villa <36754005+marcelovilla@users.noreply.github.com>
2025-06-09 13:26:38 +0200

    Improve kernel to run on interior points and use the rhs, implement kernel to init the rhs and add a benchmark group

--- gpu.jl
+++ gpu.jl
@@ -1,20 +1,66 @@
+using BenchmarkTools
+using CUDA
 using KernelAbstractions
 
-@kernel function stencil_kernel!(u, u_new)
-    M, N = size(u)
+# Both kernel functions are meant to be run only on the interior points of the grid.
+# KernelAbstractions does not seem to support arbitrary indexing, so we just add an offset
+# of 1 to the indices and skip the boundaries when specifying the ndrange.
+
+@kernel function rhs_kernel!(rhs, @Const(f), @Const(h))
     i, j = @index(Global, NTuple)
-    if i > 1 && j > 1 && i < M && j < N
-        @inbounds u_new[i, j] = 0.25 * (u[i+1, j] + u[i-1, j] + u[i,j+1] + u[i, j-1])
+    @inbounds rhs[i+1, j+1] = f(i * h, j * h)
+end
+
+@kernel function stencil_kernel!(u, u_new, @Const(rhs), @Const(h))
+    i, j = @index(Global, NTuple)
+    @inbounds u_new[i+1, j+1] = 0.25 * (u[i+2, j+1] + u[i, j+1] + u[i+1,j+2] + u[i+1, j] - h^2 * rhs[i+1, j+1])
+end
+
+function jacobi!(u, rhs, h, kernel; n_iter=4096)
+    M, N = size(u)
+    u_new = copy(u)
+    for _ in 1:n_iter
+        # Run the kernel on the interior points of the grid, skipping the boundaries
+        kernel(u, u_new, rhs, h, ndrange=(M-2, N-2))
+        u, u_new = u_new, u
     end
 end
 
-# Alternatively, one can use Apple's Metal backend
-# backend = MetalBackend()
-backend = CPU()
+function setup(N, f, backend)
+    h = 1 / (N + 1)
+    u = KernelAbstractions.zeros(backend, Float64, N, N);
 
-u = KernelAbstractions.zeros(backend, Float32, 100, 100);
-u[1,:] = u[end,:] = u[:,1] = u[:,end] .= 10.0f0
-u_new = copy(u);
+    # Only interior points are computed and boundaries are left as zero
+    rhs = KernelAbstractions.zeros(backend, Float64, N, N)
+    rhs_kernel = rhs_kernel!(backend)
+    rhs_kernel(rhs, f, h, ndrange=(N-2, N-2))
 
-kernel = stencil_kernel!(backend)
-kernel(u, u_new, ndrange=(size(u, 1), size(u, 2)))
+    stencil_kernel = stencil_kernel!(backend)
+
+    return u, rhs, h, stencil_kernel
+end
+
+# The heat equation right-hand side function
+f(x, y) = -2π^2 * sin(π * x) * sin(π * y)
+
+# Alternatively, fallback to CPU as a backend
+# backend = CPU()
+backend = CUDABackend()
+
+suite = BenchmarkGroup()
+# N = 2^14 is probably a reasonable upper limit in terms of memory usage. A grid of this
+# size and of type Float64 requires 2^14 * 2^14 * 8 bytes ≈ 2GB. We need to take into 
+# account that we will allocate three of such grids: two for the Jacobi method and one 
+# for the right-hand side.
+for p in 2:14
+    N = 2^p
+    u₀, rhs, h, stencil_kernel = setup(N, f, backend)
+    suite[N] = @benchmarkable jacobi!(u, $rhs, $h, $stencil_kernel) setup=(u = copy($u₀))
+end
+
+# We set seconds=Inf to run the benchmarks for a fixed number of samples. Otherwise, the
+# benchmarks will end as soon as the time budget is reached and at least one sample has
+# finished.
+results = run(suite, seconds=Inf, samples=50, evals=1)
+
+BenchmarkTools.save("gpu.json", results)
