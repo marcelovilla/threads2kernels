85f3fae05df259d2b4d36b39203da6f0ea0caa8f
Marcelo Villa <36754005+marcelovilla@users.noreply.github.com>
2025-06-22 13:13:20 +0200

    Remove unused arg

--- scripts/run_cpu_benchmarks.sh
+++ scripts/run_cpu_benchmarks.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for nthreads in 1 2 4 8; do
-    julia --project=. -t $nthreads ${SCRIPT_DIR}/cpu_benchmark.jl cpu
+    julia --project=. -t $nthreads ${SCRIPT_DIR}/cpu_benchmark.jl
 done
